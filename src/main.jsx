fabfc9e3c67c50334a71272550b9b6268b82fbdc
Kaif Ansari <67655725+rockxkaif@users.noreply.github.com>
2026-09-23 16:18:03 +0530

    Adapt portfolio to Elva-inspired cinematic glass with original WebGL orb

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App';
 import './index.css';
 import './glass.css';
 import './liquid.css';
+import './cinematic.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
